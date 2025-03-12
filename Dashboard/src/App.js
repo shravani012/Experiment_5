d3c418a73d20c4cf73f95dff2c797a872aa54e04
shravani012 <113746749+shravani012@users.noreply.github.com>
2025-03-13 00:24:32 +0530

    Add files via upload

---
+++ Dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserDashboard from "./components/UserDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <UserDashboard />
+    </div>
+  );
+}
+
+export default App;
+
