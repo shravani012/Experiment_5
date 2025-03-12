9e65fe54fcbccf25f2804117d3f9cd345a307e79
shravani012 <113746749+shravani012@users.noreply.github.com>
2025-03-12 22:52:11 +0530

    Add files via upload

---
+++ ContactUs/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "./components/ContactForm";
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
